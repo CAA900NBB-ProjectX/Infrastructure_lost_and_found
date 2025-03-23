be1bc06127fbc6a20fdf70331914216c22b8e412
shreyaptl <shreya.p172003@gmail.com>
2025-03-23 10:59:42 -0400

    full working docker and docker-compose

--- userdata.sh
+++ userdata.sh
@@ -14,14 +14,11 @@ sudo usermod -aG docker adminuser
 
 # Install Docker Compose v1.29.2
 DOCKER_COMPOSE_VERSION="1.29.2"
- sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" \ 
+ sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" \
  -o /usr/local/bin/docker-compose
 
 # Apply executable permissions
  sudo chmod +x /usr/local/bin/docker-compose
-
- # Create a symlink (optional, for backward compatibility)
- sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # Verify installations (optional)
  docker --version
