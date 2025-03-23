210ef70b3935d41d6c1fc35e16c309c786c3f4d8
shreyaptl <shreya.p172003@gmail.com>
2025-03-23 12:26:47 -0400

    docker container for service registry and api gateway

--- userdata.sh
+++ userdata.sh
@@ -14,15 +14,10 @@ sudo usermod -aG docker adminuser
 
 # Install Docker Compose v1.29.2
 DOCKER_COMPOSE_VERSION="1.29.2"
- sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" \
- -o /usr/local/bin/docker-compose
-
-# Apply executable permissions
- sudo chmod +x /usr/local/bin/docker-compose
-
-# Verify installations (optional)
- docker --version
- docker-compose --version
+sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" \
+-o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # Clone required repositories
  cd /home/adminuser
@@ -33,3 +28,19 @@ git clone https://github.com/CAA900NBB-ProjectX/api_gateway.git
 git clone https://github.com/CAA900NBB-ProjectX/loginservice_found_it_backend.git
 git clone https://github.com/CAA900NBB-ProjectX/itemservice_found_it_backend.git
 git clone https://github.com/CAA900NBB-ProjectX/chatservice_lost_and_found_backend.git
+
+# Move into each project and start services using docker-compose
+cd /home/adminuser/deployment/api_gateway/service-registry
+docker-compose -p app up -d
+
+cd ../api-gateway
+docker-compose -p app up -d 
+
+cd ../loginservice_found_it_backend
+docker-compose -p app up -d
+
+cd ../itemservice_found_it_backend
+docker-compose -p app up -d
+
+cd ../chatservice_lost_and_found_backend
+docker-compose -p app up -d
